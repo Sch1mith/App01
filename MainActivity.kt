af905ecff524d53cf4d2ebab9ef25810a5b14414
Sch1mith <165226331+Sch1mith@users.noreply.github.com>
2024-09-09 20:49:14 -0300

    Update MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,41 +1,51 @@
-package com.example.myapplication
+package com.example.applista
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import android.util.Log
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.applista.ui.theme.AppListaTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxSize
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var CampoTexto = rememberSaveable{
-                    mutableStateOf("")
+            AppListaTheme {
+                // A surface container using the 'background' color from the theme
+                val AzulBebe = Color(0xFFB0E0E6);
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = AzulBebe
+                ) {
+                    Greeting("Android")
+                }
             }
-
-            Log.i("AAH", CampoTexto.value)
-                
-                TextField(
-                    modifier = Modifier.background(Color.Yellow),
-                    value = CampoTexto.value,
-                    onValueChange = {
-                            Log.d("APP", it)
-                            CampoTexto.value = it
-                    }
-                )
         }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppListaTheme {
+        Greeting("Android")
     }
 }
